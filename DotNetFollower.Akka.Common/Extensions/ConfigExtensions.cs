b36cc1c896fe6251978cd5c17aea449bf3179258
.Net Follower <romancoder@gmail.com>
2017-10-30 00:02:45 -0400

    the VS solution has been added

---
+++ DotNetFollower.Akka.Common/Extensions/ConfigExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Akka.Configuration;
+
+namespace DotNetFollower.Akka.Common.Extensions
+{
+    public static class ConfigExtensions
+    {
+        public static Config SetPort(this Config config, int? port)
+        {
+            return port != null
+                ? ConfigurationFactory.ParseString($"akka.remote.helios.tcp.port = {port}").WithFallback(config)
+                : config;
+        }
+
+        public static Config SetHost(this Config config, string host)
+        {
+            return !string.IsNullOrWhiteSpace(host)
+                ? ConfigurationFactory.ParseString($"akka.remote.helios.tcp.public-hostname = {host}").WithFallback(config)
+                : config;
+        }
+
+        public static Config SetSeedNodes(this Config config, string seedNodes)
+        {
+            if (!string.IsNullOrWhiteSpace(seedNodes))
+            {
+                var seedsPatterns = seedNodes.Split(new [] {','}, StringSplitOptions.RemoveEmptyEntries);
+                var decoratedSeeds = string.Empty;
+
+                foreach (var seedStr in seedsPatterns)
+                {
+                    if (decoratedSeeds.Length > 0)
+                        decoratedSeeds += ',';
+                    decoratedSeeds += "\"" + seedStr.Trim() + "\"";
+                }
+
+                return ConfigurationFactory.ParseString($"akka.cluster.seed-nodes = [{decoratedSeeds}]").WithFallback(config);
+            }
+
+            return config;
+        }
+    }
+}
---
+++ DotNetFollower.Akka.TestApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+
+using Akka.Actor;
+using Akka.Configuration;
+
+using DotNetFollower.Akka.Common.Extensions;
+
+namespace DotNetFollower.Akka.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var actorSystemName      = ConfigurationManager.AppSettings["ACTOR_SYSTEM_NAME"];
+            var actorSystemPort      = int.Parse(ConfigurationManager.AppSettings["ACTOR_SYSTEM_PORT"]);
+            var actorSystemHost      = ConfigurationManager.AppSettings["ACTOR_SYSTEM_HOST"];
+            var actorSystemSeedNodes = ConfigurationManager.AppSettings["ACTOR_SYSTEM_SEED_NODES"];
+
+            var config = ConfigurationFactory.Load()
+                .SetPort(actorSystemPort)
+                .SetHost(actorSystemHost)
+                .SetSeedNodes(actorSystemSeedNodes);
+
+            var res = ActorSystem.Create(actorSystemName, config);
+        }
+    }
+}
